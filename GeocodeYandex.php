366db6c665b593be9b8acd9aba79e27a89e31005
calmsen <icalmsen@gmail.com>
2014-06-11 15:47:10 +1100

    Updated doc comments

--- GeocodeYandex.php
+++ GeocodeYandex.php
@@ -1,15 +1,6 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of GeocodeYandex
- *
- * @author rl
+ * @author calmsen
  */
 class GeocodeYandex {
     private $GeocodeMapsYandexUrl = "http://geocode-maps.yandex.ru/1.x/";
